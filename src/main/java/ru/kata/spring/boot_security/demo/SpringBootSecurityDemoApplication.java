f6e3d93ed7d715a426de67df5c008c8d01886bf9
Andrey Girba <andrey.girba@gmail.com>
2022-05-17 18:09:07 +0300

    before review-2

--- src/main/java/ru/kata/spring/boot_security/demo/SpringBootSecurityDemoApplication.java
+++ src/main/java/ru/kata/spring/boot_security/demo/SpringBootSecurityDemoApplication.java
@@ -2,12 +2,11 @@ package ru.kata.spring.boot_security.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SpringBootSecurityDemoApplication {
 
     public static void main(String[] args) {
-        ApplicationContext context = SpringApplication.run(SpringBootSecurityDemoApplication.class, args);
+     SpringApplication.run(SpringBootSecurityDemoApplication.class, args);
     }
 }
